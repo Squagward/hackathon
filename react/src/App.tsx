962cf6ea7932863ab0f3ff61d425d4c2ff3b966d
Cam Walter <cam.n.walter@gmail.com>
2022-10-15 18:41:01 -0500

    final attempt with react. keep in to show that we at least attempted it

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { TaskList } from "./TaskList";
 import { Timer } from "./Timer";
 
 function App() {
   return (
     <div className="App">
       <Timer />
+      <TaskList />
     </div>
   );
 }
---
+++ react/src/TaskList.tsx
@@ -0,0 +1,102 @@
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import "./TaskList.css";
+
+interface Todo {
+  text: string;
+  checked: boolean;
+}
+
+const deepClone = (object: unknown): unknown => {
+  if (object === null || typeof object !== "object") return object;
+
+  if (Array.isArray(object)) {
+    return object.map(deepClone);
+  }
+
+  if (object instanceof Object) {
+    return Object.fromEntries(
+      Object.entries(object).map(([k, v]) => [k, deepClone(v)])
+    );
+  }
+
+  throw new Error("Unsupported type to deep clone!");
+};
+
+export const TaskList = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [text, setText] = useState("");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+      }}
+    >
+      <FormGroup
+        className="todo"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        {todos.map((todo, i) => (
+          <>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={todo.checked}
+                  onClick={() => {
+                    const copy = deepClone(todos) as Todo[];
+                    copy[i].checked = !copy[i].checked;
+                    setTodos(copy);
+                  }}
+                />
+              }
+              label={todo.text}
+            />
+            <Button
+              variant="contained"
+              onClick={() => {
+                setTodos((prev) =>
+                  prev.filter(({ text }) => text !== todo.text)
+                );
+              }}
+            >
+              Delete
+            </Button>
+          </>
+        ))}
+      </FormGroup>
+      <div>
+        <TextField
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          value={text}
+          placeholder="Todo..."
+        />
+        <Button
+          variant="outlined"
+          onClick={() => {
+            setTodos((prev) => prev.concat({ text, checked: false }));
+            setText("");
+          }}
+        >
+          Add todo
+        </Button>
+      </div>
+    </div>
+  );
+};
