5e648513d39125c7c6321821cfd36e3e78a0c029
Aidan Aalund <aidanaalund@icloud.com>
2022-10-15 17:55:17 -0500

    removed settings.js, wrote script in an improved settings.html.

--- settings.js
+++
@@ -1,7 +0,0 @@
-function getInputValue() {
-  // Selecting the input element and get its value
-  var inputVal = document.getElementById("whitelist").value;
-
-  // Displaying the value
-  alert(inputVal);
-}
