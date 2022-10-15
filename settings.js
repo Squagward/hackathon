daf06f3ff3425424c32a24abb0062763af18f337
Aidan Aalund <aidanaalund@icloud.com>
2022-10-15 17:40:08 -0500

    Added settings.html and settings.js

---
+++ settings.js
@@ -0,0 +1,7 @@
+function getInputValue() {
+  // Selecting the input element and get its value
+  var inputVal = document.getElementById("whitelist").value;
+
+  // Displaying the value
+  alert(inputVal);
+}
