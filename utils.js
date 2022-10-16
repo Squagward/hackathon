71863c53024e6c054e8c40a585a78c0fe02daed1
Aidan Aalund <aidanaalund@icloud.com>
2022-10-16 13:26:47 -0500

    finalize project

--- utils.js
+++
@@ -1,7 +0,0 @@
-export function getInputValue() {
-  // Selecting the input element and get its value
-  var inputVal = document.getElementById("whitelist").value;
-
-  // Displaying the value
-  return inputVal.split("\n");
-}
